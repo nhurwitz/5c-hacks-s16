d25dfdd12477b6cd167b3fdc0d33ceaca75966fa
Noam Hurwitz <nhurwitz@gmail.com>
2016-04-15 21:15:12 -0700

    Action Type

--- server/action.go
+++ server/action.go
@@ -1,1 +1,15 @@
 package server
+
+type ActionEnum string
+
+// Possible Actions
+const (
+	SPAWN     ActionEnum = "Spawn"     // snake spawns
+	DIRECTION ActionEnum = "Direction" // snake changes direction
+	QUIT      ActionEnum = "Quit"      // snake quits
+)
+
+type Action struct {
+	actionType ActionEnum
+	snakeId    string
+}
