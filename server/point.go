ae3ef5112386945234177d63afae1620f5169c02
Antony Bello <antony.bello@pomona.edu>
2016-04-15 20:56:25 -0700

    points/direction

--- server/point.go
+++ server/point.go
@@ -1,1 +1,37 @@
 package server
+
+type point struct {
+	x int
+	y int
+	z int
+}
+
+// Direction = The way the snake is heading
+type Direction string
+
+// 6 Directions
+const (
+	NORTH Direction = "North"
+	EAST  Direction = "East"
+	SOUTH Direction = "South"
+	WEST  Direction = "West"
+	UP    Direction = "Up"
+	DOWN  Direction = "Down"
+)
+
+func move(p point, d Direction) {
+	switch d {
+	case NORTH:
+		p.y++
+	case EAST:
+		p.x++
+	case SOUTH:
+		p.y--
+	case WEST:
+		p.x--
+	case UP:
+		p.z++
+	case DOWN:
+		p.z--
+	}
+}
