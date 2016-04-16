fc1c026e785c51bdd56b2ece1f04a7ac01f005e0
Antony Bello <antony.bello@pomona.edu>
2016-04-15 21:09:03 -0700

    initial snakey

--- server/snake.go
+++ server/snake.go
@@ -1,3 +1,25 @@
 package server
 
-type snake struct{}
+// Snake = ~~~:)
+type Snake struct {
+	head      Point
+	tail      []Point
+	direction Direction
+	id        string
+}
+
+func (snake Snake) collidedWithEdge(sideLength int) bool {
+	return false
+}
+
+func (snake Snake) collidedWithSelf() bool {
+	return false
+}
+
+func (snake Snake) collidedWithOther(other Snake) bool {
+	return false
+}
+
+func (snake Snake) move(capturing bool) Snake {
+	return snake
+}
